b8147b9449f56a699408329b676fcf6b8396191d
FlMonkey <75172047+FlMonkey@users.noreply.github.com>
2023-03-10 09:17:40 +0100

    works now
    TODO: add a check to see if they already inputed that coord

--- battleship.py
+++ battleship.py
@@ -1,40 +1,68 @@
-import random
-import math
+import random, math, copy, time
+from colorama import Fore
+
 
 def createBoard(dimens):
+    
+    global counter
+    counter = 0
     board=[["-"]*dimens for j in range(dimens)]
     for j in range(dimens):
         board[0][j]=j
         board[j][0]=j
-    board1 = board
+    board1 = copy.deepcopy(board)
     #place ships on board
-    usedr=[]
-    usedc=[]
+    usedr=set()
+    usedc=set()
     for i in range(math.ceil(dimens/2)):
-        rowss = random.randint(1,dimens)
-        columss = random.randint(1,dimens)
-        if rowss[i] != usedr[i] and columss[i] != usedc[i]:
-            board1[rowss][columss]="S"
-            usedr.append(rowss)
-            usedc.append(columss)
-    return board, board1 
-
+        rowss = random.randint(1, dimens-1)
+        columss = random.randint(1, dimens-1)
+        while (rowss, columss) in zip(usedr, usedc):
+            rowss = random.randint(0, dimens-1)
+            columss = random.randint(0, dimens-1)
+        board1[rowss][columss] = "S"
+        counter = counter + 1
+        usedr.add(rowss)
+        usedc.add(columss)
+    return board, board1
 
 
 def printBoard(board):
   for b in board:
       print(*b)
 
-myBoard, board1 = createBoard(10)
+def setup():
+    global myBoard, board1
+    myBoard, board1 = createBoard(5)
 
-printBoard(myBoard)
-print("\n")
+    printBoard(myBoard)
+    print("\n")
 
 def enterguess():
-    column = int(input("What collumn do you want to place your ship? "))
+    global counter
     row = int(input("What row do you want to place your ship? "))
+    column = int(input("What collumn do you want to place your ship? "))
+    
 
-    myBoard[row][column]="X"
+    if board1[row][column]=="S":
+        myBoard[row][column]="O"
+        printBoard(myBoard)
+        counter = counter - 1
+        
+    else:
+        myBoard[row][column]="X"
+        printBoard(myBoard)
+    
+while True:
+    
+    myBoard, board1 = createBoard(5)
+
     printBoard(myBoard)
-
-enterguess()
+    print("\n")
+    
+    while counter > 0:
+        enterguess()
+    if counter == 0:
+        print(Fore.GREEN + "you win")
+        print(Fore.RESET)
+        time.sleep(1.5)
